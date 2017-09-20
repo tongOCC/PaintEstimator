f0f58fa33cc0de1edf69c1e638180f6a889a1702
TongOCC <kuroneko.lelouch@gmail.com>
2017-09-19 17:48:44 -0700

    did alot

--- app/src/main/java/com/example/tong/paintestimator/HelpActivity.java
+++ app/src/main/java/com/example/tong/paintestimator/HelpActivity.java
@@ -3,13 +3,17 @@ package com.example.tong.paintestimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class HelpActivity extends AppCompatActivity {
-
+private TextView mEstimatedGallons;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mEstimatedGallons= (TextView) findViewById(R.id.EstimateGallonsTextView) ;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
     }
     public void returnToMainOnClick(View v)
     {
